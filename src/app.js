95ed4297c46d66a96c0e92fcf2ae6493624e8d0e
Félix Jiménez Boza <106713963+felixjbo@users.noreply.github.com>
2023-04-17 00:38:05 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/4geeks.ico";*/
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function excuse() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let whoN = who[Math.floor(Math.random() * who.length) + 1];
+    let actionN = action[Math.floor(Math.random() * who.length) + 1];
+    let whatN = what[Math.floor(Math.random() * what.length) + 1];
+    let whenN = when[Math.floor(Math.random() * when.length) + 1];
+
+    return whoN + " " + actionN + " " + whatN + " " + whenN;
+  }
 };
